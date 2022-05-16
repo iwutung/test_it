fe86e931151f735d65bf4d3cfc3a55272230ca54
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-05-16 09:28:59 +0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# test_it
+readme
---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+/**
+putchar function
+returns 1 if success, else 0
+*/
+
+#include <unistd.h>
+
+int _putc(char c)
+{
+    return (write(1, &c, 1));
+}
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i = 0;
+
+    va_start(args, format);
+
+    while (format[i] != '\0')
+    {
+        if (format[i] != '%')
+        {
+            _putc(format[i]);
+        }
+        else if (format[i] == '%')
+        {
+            if (format[i+1] == 'c')
+            {
+                _putc(va_arg(args, int));
+                i++;
+            }
+        }
+        i++;
+    }
+
+    va_end(args);
+
+    return (0);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    _printf("Hello worl%c\n", '1');
+    printf("Hello 100%%\n");
+
+    return (0);
+}
